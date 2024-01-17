0db64889326199ffb7e910598685bfd2c5acc071
Jacob Sendzik <jake.sendzik@gmail.com>
2024-01-17 13:42:19 -0500

    Complete

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 1:
+        print(i)
+        i -= 1
+    if i == 0:
+        print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squares = [num * num for num in int_list]
+    return squares
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    def fb(num):
+        if num % 3 == 0 and num % 5 == 0:
+            return "FizzBuzz"
+        elif num % 3 == 0:
+            return "Fizz"
+        elif num % 5 == 0:
+            return "Buzz"
+        else:
+            return str(num)
+
+    for num in range(1, 101):
+        print(fb(num))
